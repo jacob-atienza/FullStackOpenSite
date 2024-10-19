73e02baba1cfb5930d112ff5b55b4ffe3d626cc2
Jacob Atienza <Jatienza2932@conestogac.on.ca>
2024-10-19 14:51:22 -0400

    FullstackOhNo

---
+++ part3/index.js
@@ -0,0 +1,15 @@
+﻿const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
