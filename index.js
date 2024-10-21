136f4a603e54ae58d2621bbf1bcbfe24e91469a5
Jacob Atienza <Jatienza2932@conestogac.on.ca>
2024-10-21 01:18:19 -0400

    lets see

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ﻿const express = require('express')
 const app = express()
 require('dotenv').config()
-
+app.use(express.static('dist'))
 const Person = require('./models/person')
 
 let people = []
