2fba3b8191fd4a342fd41c992e1c028cae78c9b4
Jacob Atienza <Jatienza2932@conestogac.on.ca>
2024-10-21 02:02:44 -0400

    lets see

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 ﻿const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const Person = require('./models/person.js')
+
+if (!process.env.MONGODB_URI) {
+  console.error('MONGODB_URI is not set')
+}
+console.log('Environment Variables:', process.env)
+const Person = require('./models/person')
 
 const app = express()
 app.use(cors())
